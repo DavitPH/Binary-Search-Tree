bbdd596e91cfe3e90a2545f945062c6c3fc8ac67
DavitPH <davitph0@gmail.com>
2022-12-29 17:17:17 +0700

    Memperbaiki posisi class node

--- Binary Search Tree/Program.cs
+++ Binary Search Tree/Program.cs
@@ -6,21 +6,26 @@ using System.Threading.Tasks;
 
 namespace Binary_Search_Tree
 {
+    class Node
+    {
+        public string info;
+        public Node leftchild;
+        public Node rightchild;
+
+
+        public Node(string i, Node l, Node r)
+        {
+            info = i;
+            leftchild = l;
+            rightchild = r;
+        }
+    }
     class Program
     {
-        class Node
+        public Node ROOT;
+        public program()
         {
-            public string info;
-            public Node leftchild;
-            public Node rightchild;
-
-
-            public Node(string i, Node l, Node r)
-            {
-                info = i;
-                leftchild = l;
-                rightchild = r;
-            }
+            ROOT = null;
         }
 
         static void Main(string[] args)
