47714d3cd8a90e286713d919465801e8fce70a98
DavitPH <davitph0@gmail.com>
2022-12-29 17:15:47 +0700

    Class Node

--- Binary Search Tree/Program.cs
+++ Binary Search Tree/Program.cs
@@ -8,6 +8,21 @@ namespace Binary_Search_Tree
 {
     class Program
     {
+        class Node
+        {
+            public string info;
+            public Node leftchild;
+            public Node rightchild;
+
+
+            public Node(string i, Node l, Node r)
+            {
+                info = i;
+                leftchild = l;
+                rightchild = r;
+            }
+        }
+
         static void Main(string[] args)
         {
         }
